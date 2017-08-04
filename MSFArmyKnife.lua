d2242ff434a9baafa22ef777ae04b01101420bb5
DarkRonius <garymarkwatts@gmail.com>
2017-08-04 18:31:00 +0100

    Added PresdsTicks

--- MSFArmyKnife.lua
+++ MSFArmyKnife.lua
@@ -1,44 +1,60 @@
 local e={}
 
+local gameTicks = 0
+local pressTicks = 0
+
+local PRESS_TICK_DELAY = 30 -- = 0.5 seconds
+
 function e.OnAllocate()end
 function e.OnInitialize()end
 
 function e.Update()
 
-	if (bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.DOWN)==PlayerPad.DOWN and bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.CALL)==PlayerPad.CALL)then
+	if(pressTicks < gameTicks)then
 
-		TppMission.UpdateCheckPointAtCurrentPosition()
+		if (bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.DOWN)==PlayerPad.DOWN and bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.CALL)==PlayerPad.CALL)then
+
+			TppMission.UpdateCheckPointAtCurrentPosition()
+			
+			TppUiCommand.AnnounceLogView("140.96: Mission Data Saved")
+			
+			TppUiCommand.AnnounceLogView("The tongues of dying men force attention, like deep harmony.") 
+			
+			TppUiCommand.AnnounceLogView("Where words are spent, they are seldom spent in vain.")
+
+			pressTicks = gameTicks + PRESS_TICK_DELAY
+
+		end
 		
-		TppUiCommand.AnnounceLogView("140.96: Mission Data Saved")
+		if (bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.LEFT)==PlayerPad.LEFT and bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.CALL)==PlayerPad.CALL)then
+
+			TppUiCommand.AnnounceLogView("I want to get off Mr Bones Wild Ride")
+			
+			Player.ResetPadMask {
+			settingName = "limitMove"
+			}
+
+			Player.ResetPadMask {
+			settingName = "restrictAttacks"
+			}
+
+			pressTicks = gameTicks + PRESS_TICK_DELAY
+
+		end
+
+		if (bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.UP)==PlayerPad.UP and bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.CALL)==PlayerPad.CALL)then
+
+			Player.ResetLifeMaxValue()
+			
+			TppUiCommand.AnnounceLogView("Your Life/Psyche is looking good, Snake!")
+
+			pressTicks = gameTicks + PRESS_TICK_DELAY
+
+		end
 		
-		TppUiCommand.AnnounceLogView("The tongues of dying men force attention, like deep harmony.") 
-		
-		TppUiCommand.AnnounceLogView("Where words are spent, they are seldom spent in vain.")
+		gameTicks = gameTicks + 1
 
 	end
-	
-	if (bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.LEFT)==PlayerPad.LEFT and bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.CALL)==PlayerPad.CALL)then
-
-		TppUiCommand.AnnounceLogView("I want to get off Mr Bones Wild Ride")
-		
-		Player.ResetPadMask {
-		settingName = "limitMove"
-		}
-
-		Player.ResetPadMask {
-		settingName = "restrictAttacks"
-		}
-
-	end
-
-	if (bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.UP)==PlayerPad.UP and bit.band(PlayerVars.scannedButtonsDirect,PlayerPad.CALL)==PlayerPad.CALL)then
-
-		Player.ResetLifeMaxValue()
-		
-		TppUiCommand.AnnounceLogView("Your Life/Psyche is looking good, Snake!")
-
-	end
-	
 end
 
 function e.OnTerminate()end
